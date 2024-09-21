fd43ad4d27e718c6ddaed6228eda0751168a05c0
jjuya21 <jjuya21@gmail.com>
2024-09-22 03:13:01 +0900

    feat: PointService 추가

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,45 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class PointService {
+
+    private final PointHistoryTable pointHistoryTable;
+    private final UserPointTable userPointTable;
+
+    public PointService(PointHistoryTable pointHistoryTable, UserPointTable userPointTable) {
+
+        this.pointHistoryTable = Objects.requireNonNull(pointHistoryTable);
+        this.userPointTable = Objects.requireNonNull(userPointTable);
+    }
+
+    public UserPoint getPoint(Long id) {
+
+        return userPointTable.selectById(id);
+    }
+
+    public List<PointHistory> getPointHistory(Long id) {
+
+        return pointHistoryTable.selectAllByUserId(id);
+    }
+
+    public UserPoint addPoint(Long id, long amount) {
+
+        UserPoint userPoint = getPoint(id);
+
+        return userPointTable.insertOrUpdate(userPoint.id(), userPoint.point() + amount);
+    }
+
+    public UserPoint usePoint(Long id, long amount) {
+
+        UserPoint userPoint = getPoint(id);
+
+        return userPointTable.insertOrUpdate(userPoint.id(), userPoint.point() - amount);
+    }
+}
