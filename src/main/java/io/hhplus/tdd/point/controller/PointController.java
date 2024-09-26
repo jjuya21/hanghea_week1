d302571ede5c83d600eac2493e85e3f6a3ed06da
jjuya21 <jjuya21@gmail.com>
2024-09-27 02:28:07 +0900

    refactor: Service에서 domain을 받도록 수정

--- src/main/java/io/hhplus/tdd/point/controller/PointController.java
+++ src/main/java/io/hhplus/tdd/point/controller/PointController.java
@@ -2,6 +2,7 @@ package io.hhplus.tdd.point.controller;
 
 import io.hhplus.tdd.point.dto.PointHistoryDto;
 import io.hhplus.tdd.point.dto.UserPointDto;
+import io.hhplus.tdd.point.entity.UserPoint;
 import io.hhplus.tdd.point.service.PointService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -28,21 +30,27 @@ public class PointController {
     public ResponseEntity<UserPointDto> point(@PathVariable("id") long id) {
 
         log.info("GET /point/{} - Retrieving point for user with ID: {}", id, id);
-        UserPointDto userPoint = pointService.getPoint(id);
+        UserPoint userPoint = pointService.getPoint(UserPointDto.builder()
+                .id(id)
+                .build());
         log.info("Retrieved point for user ID {}: {}", id, userPoint.getPoint());
 
-        return ResponseEntity.ok(userPoint);
+        return ResponseEntity.ok(UserPointDto.from(userPoint));
     }
 
     /**
      * 특정 유저의 포인트 충전/이용 내역 조회 기능
      */
     @GetMapping("{id}/histories")
-    public ResponseEntity<List<PointHistoryDto>> history(@PathVariable("id") long id) {
+    public ResponseEntity<List<PointHistoryDto>> history(@PathVariable("id") long userId) {
 
-        log.info("GET /point/{}/histories - Retrieving point histories for user with ID: {}", id, id);
-        List<PointHistoryDto> histories = pointService.getPointHistory(id);
-        log.info("Retrieved {} point history entries for user ID: {}", histories.size(), id);
+        log.info("GET /point/{}/histories - Retrieving point histories for user with ID: {}", userId, userId);
+        List<PointHistoryDto> histories = pointService.getPointHistory(PointHistoryDto.builder()
+                        .userId(userId).build())
+                .stream()
+                .map(PointHistoryDto::from)
+                .collect(Collectors.toList());
+        log.info("Retrieved {} point history entries for user ID: {}", histories.size(), userId);
 
         return ResponseEntity.ok(histories);
     }
@@ -54,10 +62,13 @@ public class PointController {
     public ResponseEntity<UserPointDto> charge(@PathVariable("id") long id, @RequestBody long amount) {
 
         log.info("PATCH /point/{}/charge - Charging {} points for user with ID: {}", id, amount, id);
-        UserPointDto updatedUserPoint = pointService.chargePoint(id, amount);
+        UserPoint updatedUserPoint = pointService.chargePoint(UserPointDto.builder()
+                .id(id)
+                .amount(amount)
+                .build());
         log.info("User ID {} now has {} points after charging {} points", id, updatedUserPoint.getPoint(), amount);
 
-        return ResponseEntity.ok(updatedUserPoint);
+        return ResponseEntity.ok(UserPointDto.from(updatedUserPoint));
     }
 
     /**
@@ -67,9 +78,12 @@ public class PointController {
     public ResponseEntity<UserPointDto> use(@PathVariable("id") long id, @RequestBody long amount) {
 
         log.info("PATCH /point/{}/use - Using {} points for user with ID: {}", id, amount, id);
-        UserPointDto updatedUserPoint = pointService.usePoint(id, amount);
+        UserPoint updatedUserPoint = pointService.usePoint(UserPointDto.builder()
+                .id(id)
+                .amount(amount)
+                .build());
         log.info("User ID {} now has {} points after using {} points", id, updatedUserPoint.getPoint(), amount);
 
-        return ResponseEntity.ok(updatedUserPoint);
+        return ResponseEntity.ok(UserPointDto.from(updatedUserPoint));
     }
 }
